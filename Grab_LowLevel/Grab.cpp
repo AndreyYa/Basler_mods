cba7eb6ab688df8210c4a629a65328dec3ebee7e
AndreyYa <andrey.yatsunenko@gmail.com>
2014-08-15 20:50:18 -0700

    2 cameras work

--- Grab_LowLevel/Grab.cpp
+++ Grab_LowLevel/Grab.cpp
@@ -7,6 +7,36 @@ using namespace std;
 
 typedef CBaslerUsbCamera Camera_t;
 
+//static const uint32_t c_countOfImagesToGrab = 20;
+static const size_t c_maxCamerasToUse = 2;
+const int numGrabs = 20;
+const int numBuffers = 20;
+
+static vector<int64_t> c_PrevTime(c_maxCamerasToUse, 0);
+static vector<int64_t> c_CurrTime(c_maxCamerasToUse, 0);
+//int64_t PrevTime = 0;
+//int64_t CurrTime = 0;
+
+static vector<Camera_t*> c_CamArray(c_maxCamerasToUse,0);
+static vector<CBaslerUsbCamera::StreamGrabber_t*> c_GrabberArray(c_maxCamerasToUse, 0);
+static vector<GrabResult*> c_GrabResArray(c_maxCamerasToUse, 0);
+static vector<IChunkParser*> c_ChunkParserArray(c_maxCamerasToUse, 0);
+
+
+static vector<vector<unsigned char*>> c_BuffersArray(c_maxCamerasToUse, vector<unsigned char*>(numBuffers, 0));
+//unsigned char* ppBuffers[numBuffers];
+static vector<vector<StreamBufferHandle>> c_BufferHandlesArray(c_maxCamerasToUse, vector<StreamBufferHandle>(numBuffers, 0));
+//StreamBufferHandle handles[numBuffers];
+
+struct MyContext
+{
+	// Define some application specific context information here
+};
+static vector<vector<MyContext*>> c_MyContextArray(c_maxCamerasToUse, vector<MyContext*>(numBuffers, 0));
+//MyContext context[numBuffers];
+
+
+
 void ProcessImage(unsigned char* pImage, int imageSizeX, int imageSizeY)
 {
 	//cout << "Camera: x" << imageSizeX << " Y: " << imageSizeY << endl;
@@ -14,19 +44,15 @@ void ProcessImage(unsigned char* pImage, int imageSizeX, int imageSizeY)
 	// Do something with the image data
 }
 
-struct MyContext
-{
-	// Define some application specific context information here
-};
 
 int main()
 {
 	PylonAutoInitTerm autoInitTerm;
-	const int numGrabs = 30;
+	//const int numGrabs = 30;
 
 	try
 	{
-		// Enumerate GigE cameras
+		// Enumerate	cameras
 		CTlFactory& TlFactory = CTlFactory::GetInstance();
 		ITransportLayer *pTl = TlFactory.CreateTl(Camera_t::DeviceClass());
 		DeviceInfoList_t devices;
@@ -36,153 +62,161 @@ int main()
 		}
 
 		// Create a camera object
-		Camera_t Camera(pTl->CreateDevice(devices[0]));
-
-		// Open the camera object
-		Camera.Open();
-
-		// Parameterize the camera
-
-		// Mono8 pixel format
-		Camera.PixelFormat.SetValue(PixelFormat_Mono8);
-
-		// Maximized AOI
-		Camera.OffsetX.SetValue(0);
-		Camera.OffsetY.SetValue(0);
-		Camera.Width.SetValue(Camera.Width.GetMax());
-		Camera.Height.SetValue(Camera.Height.GetMax());
-
-		// Continuous mode, no external trigger used
-		Camera.TriggerSelector.SetValue(TriggerSelector_FrameBurstStart);
-		Camera.TriggerMode.SetValue(TriggerMode_On);
-		Camera.TriggerSource.SetValue(TriggerSource_Software);
-
-		
-		Camera.AcquisitionMode.SetValue(AcquisitionMode_Continuous);
-		Camera.AcquisitionBurstFrameCount.SetValue(numGrabs);
-
-
-		// Configure exposure time and mode
-		Camera.ExposureMode.SetValue(ExposureMode_Timed);
-		Camera.ExposureTime.SetValue(100);
-
-		// check whether stream grabbers are avalaible
-		if (Camera.GetNumStreamGrabberChannels() == 0) {
-			cerr << "Camera doesn't support stream grabbers." << endl;
+		for (int i = 0; i < devices.size(); i++)
+		{
+			c_CamArray[i] = new Camera_t(pTl->CreateDevice(devices[i]));
+			c_GrabResArray[i] = new GrabResult();
 		}
-		else {
-			// Get and open a stream grabber
-			IStreamGrabber* pGrabber = Camera.GetStreamGrabber(0);
-			CBaslerUsbCamera::StreamGrabber_t StreamGrabber(Camera.GetStreamGrabber(0));
-			StreamGrabber.Open();
+		
 
-			if (GenApi::IsWritable(Camera.ChunkModeActive)) {
-				Camera.ChunkModeActive.SetValue(true);
+
+		for (int i = 0; i < c_CamArray.size(); i++)
+		{
+			c_CamArray[i]->Open();
+			c_CamArray[i]->PixelFormat.SetValue(PixelFormat_Mono8);
+			// Maximized AOI
+			c_CamArray[i]->OffsetX.SetValue(0);
+			c_CamArray[i]->OffsetY.SetValue(0);
+			c_CamArray[i]->Width.SetValue(c_CamArray[i]->Width.GetMax());
+			c_CamArray[i]->Height.SetValue(c_CamArray[i]->Height.GetMax());
+			// Continuous mode, no external trigger used
+			c_CamArray[i]->TriggerSelector.SetValue(TriggerSelector_FrameBurstStart);
+			c_CamArray[i]->TriggerMode.SetValue(TriggerMode_On);
+			c_CamArray[i]->TriggerSource.SetValue(TriggerSource_Software);
+
+			c_CamArray[i]->AcquisitionMode.SetValue(AcquisitionMode_Continuous);
+			c_CamArray[i]->AcquisitionBurstFrameCount.SetValue(numGrabs);
+			// Configure exposure time and mode
+			c_CamArray[i]->ExposureMode.SetValue(ExposureMode_Timed);
+			c_CamArray[i]->ExposureTime.SetValue(100);
+
+			if (GenApi::IsWritable(c_CamArray[i]->ChunkModeActive)) {
+				c_CamArray[i]->ChunkModeActive.SetValue(true);
 			}
-			else {
+			else 
+			{
 				cerr << "The camera does not support chunk features" << endl;
 				return 1;
 			}
-			Camera.ChunkSelector.SetValue(ChunkSelector_Timestamp);
-			Camera.ChunkEnable.SetValue(true);
-			// Create ChunkParser
-			IChunkParser &ChunkParser = *Camera.CreateChunkParser();
+
+			c_CamArray[i]->ChunkSelector.SetValue(ChunkSelector_Timestamp);
+			c_CamArray[i]->ChunkEnable.SetValue(true);
+			c_ChunkParserArray[i] = c_CamArray[i]->CreateChunkParser();
+		}
 
 
-			// Parameterize the stream grabber
-			const int bufferSize = (int)Camera.PayloadSize();
-			const int numBuffers = 10;
-			StreamGrabber.MaxBufferSize = bufferSize;
-			StreamGrabber.MaxNumBuffer = numBuffers;
-			StreamGrabber.PrepareGrab();
-
-
-
-			// Allocate and register image buffers, put them into the
-			// grabber's input queue
-			unsigned char* ppBuffers[numBuffers];
-			MyContext context[numBuffers];
-			StreamBufferHandle handles[numBuffers];
-			for (int i = 0; i < numBuffers; ++i)
-			{
-				ppBuffers[i] = new unsigned char[bufferSize];
-				handles[i] = StreamGrabber.RegisterBuffer(ppBuffers[i], bufferSize);
-				StreamGrabber.QueueBuffer(handles[i], &context[i]);
+		for (int i = 0; i < c_CamArray.size(); i++)
+		{
+			// check whether stream grabbers are avalaible
+			if (c_CamArray[i]->GetNumStreamGrabberChannels() == 0) {
+				cerr << "Camera doesn't support stream grabbers." << endl;
 			}
+			else
+			{
+				CBaslerUsbCamera::StreamGrabber_t* pGrabber = new CBaslerUsbCamera::StreamGrabber_t(c_CamArray[i]->GetStreamGrabber(0));
+				pGrabber->Open();
+				c_GrabberArray[i] = pGrabber;
 
+				// Parameterize the stream grabber
+				const int bufferSize = (int)c_CamArray[i]->PayloadSize();
+				c_GrabberArray[i]->MaxBufferSize = bufferSize;
+				c_GrabberArray[i]->MaxNumBuffer = numBuffers;
+				c_GrabberArray[i]->PrepareGrab();
+
+				for (int j = 0; j < numBuffers; ++j)
+				{					
+					c_BuffersArray[i][j] = new unsigned char[bufferSize];
+					c_BufferHandlesArray[i][j] = c_GrabberArray[i]->RegisterBuffer((void*)c_BuffersArray[i][j], bufferSize);
+					c_GrabberArray[i]->QueueBuffer(c_BufferHandlesArray[i][j], c_MyContextArray[i][j]);
+				}
+			}
+		}
+		
+		for (int i = 0; i < c_CamArray.size(); i++)
+		{
 			// Start image acquisition
-			Camera.AcquisitionStart.Execute();
+			c_CamArray[i]->AcquisitionStart.Execute();
+		}
 
-			// Grab and process 100 images
-			
-			GrabResult Result;
-			int64_t PrevTime = 0;
-			int64_t CurrTime = 0;
+		
 
-			Camera.TriggerSoftware.Execute();
+		for (int i = 0; i < c_CamArray.size(); i++)
+		{
+			// Start image acquisition
+			c_CamArray[i]->TriggerSoftware.Execute();
+		}
 
-			for (int i = 0; i < numGrabs; ++i) {
-				// Wait for the grabbed image with a timeout of 3 seconds
-				
-				if (StreamGrabber.GetWaitObject().Wait(3000)) {
+		for (int j = 0; j < numGrabs; ++j) 
+		{		
+			for (int i = 0; i < c_CamArray.size(); i++)
+			{
+				if (c_GrabberArray[i]->GetWaitObject().Wait(3000)) 
+				{
 					// Get an item from the grabber's output queue
-					if (!StreamGrabber.RetrieveResult(Result)) {
+					if (!c_GrabberArray[i]->RetrieveResult(*c_GrabResArray[i])) {
 						cerr << "Failed to retrieve an item from the output queue" << endl;
 						break;
 					}
-					if (Result.Succeeded()) {
-						// Grabbing was successful. Process the image.
-						ChunkParser.AttachBuffer(Result.Buffer(), Result.GetPayloadSize());
 
-						if (IsReadable(Camera.ChunkTimestamp))
+					if (c_GrabResArray[i]->Succeeded()) 
+					{
+						// Grabbing was successful. Process the image.
+						c_ChunkParserArray[i]->AttachBuffer(c_GrabResArray[i]->Buffer(), c_GrabResArray[i]->GetPayloadSize());
+
+						if (IsReadable(c_CamArray[i]->ChunkTimestamp))
 						{
-							CurrTime = Camera.ChunkTimestamp.GetValue();
-							cout << "TimeStamp : " << (CurrTime*0.000000001) << " dT:" << ((CurrTime - PrevTime)*0.000000001) << endl;
-							PrevTime = CurrTime;
+							c_CurrTime[i] = c_CamArray[i]->ChunkTimestamp.GetValue();
+							cout << "TimeStamp Cam"<< i << ": " << (c_CurrTime[i] * 0.000000001) << " dT:" << ((c_CurrTime[i] - c_PrevTime[i])*0.000000001) << endl;
+							c_PrevTime[i] = c_CurrTime[i];
 						}
-						ProcessImage((unsigned char*)Result.Buffer(), Result.GetSizeX(), Result.GetSizeY());
+						ProcessImage((unsigned char*)c_GrabResArray[i]->Buffer(), c_GrabResArray[i]->GetSizeX(), c_GrabResArray[i]->GetSizeY());
 					}
-					else {
-						cerr << "Grab failed: " << Result.GetErrorDescription() << endl;
+					else 
+					{
+						cerr << "Grab failed: " << c_GrabResArray[i]->GetErrorDescription() << endl;
 						break;
 					}
 					// Requeue the buffer
 					if (i + numBuffers < numGrabs)
-						StreamGrabber.QueueBuffer(Result.Handle(), Result.Context());
+						c_GrabberArray[i]->QueueBuffer(c_GrabResArray[i]->Handle(), c_GrabResArray[i]->Context());
 				}
-				else {
+				else 
+				{
 					cerr << "timeout occurred when waiting for a grabbed image" << endl;
 					break;
 				}
 			}
+		}
 
-			// Finished. Stop grabbing and do clean-up
+		// Finished. Stop grabbing and do clean-up
+		for (int i = 0; i < c_CamArray.size(); i++)
+		{
+			// Start image acquisition
+			c_CamArray[i]->AcquisitionStop.Execute();
 
-			// The camera is in continuous mode, stop image acquisition
-			Camera.AcquisitionStop.Execute();
-
-			// Flush the input queue, grabbing may have failed
-			StreamGrabber.CancelGrab();
-
-			// Consume all items from the output queue
-			while (StreamGrabber.GetWaitObject().Wait(0)) {
-				StreamGrabber.RetrieveResult(Result);
-				if (Result.Status() == Canceled)
+			while (c_GrabberArray[i]->GetWaitObject().Wait(0)) 
+			{
+				c_GrabberArray[i]->RetrieveResult(*c_GrabResArray[i]);
+				if (c_GrabResArray[i]->Status() == Canceled)
 					cout << "Got canceled buffer" << endl;
 			}
 
 			// Deregister and free buffers
-			for (int i = 0; i < numBuffers; ++i) {
-				StreamGrabber.DeregisterBuffer(handles[i]);
-				delete[] ppBuffers[i];
+			for (int j = 0; j < numBuffers; ++j) 
+			{
+				//c_GrabberArray[i]->DeregisterBuffer(handles[j]);
+				c_GrabberArray[i]->DeregisterBuffer(c_BufferHandlesArray[i][j]);
+					
+				//delete[] ppBuffers[j];
+				delete[] c_BuffersArray[i][j];
 			}
 
-			// Clean up
-			StreamGrabber.FinishGrab();
-			StreamGrabber.Close();
-		}
+			c_GrabberArray[i]->FinishGrab();
+			c_GrabberArray[i]->Close();
 
-		Camera.Close();
+			c_CamArray[i]->Close();
+		}
+			
 		TlFactory.ReleaseTl(pTl);
 	}
 	catch (GenICam::GenericException &e)
